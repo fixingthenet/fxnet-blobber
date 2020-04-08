83bab861ca8f5467bad8cf7079d8851bf11ac2af
Peter Schrammel <peter.schrammel@gmx.de>
2020-04-08 22:29:55 +0200

    loading pages from attachments

--- src/transformations/inBrowser.js
+++ src/transformations/inBrowser.js
@@ -13,15 +13,43 @@ class Trans extends Base {
     }
 
     async transform() {
-        await captureWebsite.file('https://heise.de/', this.outPath, {
-              launchOptions: {
-                  args: [
-                      '--no-sandbox',
-                      '--disable-setuid-sandbox'
-                  ]
-              }
-          })
+        var url = new URL(this.options.url)
+
+        switch(url.protocol) {
+        case 'att:':
+            var html = await fs.promises.readFile(this.trans.attPath(url.hostname))
+            await captureWebsite.file(html.toString(), this.outPath, {
+                type: 'jpeg',
+                inputType: 'html',
+                fullPage: true,
+                launchOptions: {
+                    args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox'
+                    ]
+                }
+            })
+            break;
+        case 'http:':
+        case 'https:':
+            await captureWebsite.file(this.options.url, this.outPath, {
+                type: 'jpeg',
+                fullPage: true,
+                launchOptions: {
+                    args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox'
+                    ]
+                }
+            })
+            break;
+        default:
+            throw("Unknown protocol ", url.protocol)
+        }
+        return this.options.m
     }
+
+
 }
 
 module.exports = [NAME, Trans]
